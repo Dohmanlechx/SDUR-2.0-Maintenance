277ba5096e97ba952cc620334def6c31676428d7
David Öhman <dohman_92@hotmail.com>
2020-04-04 15:31:47 +0200

    Fix logic for new radio buttons (For TargetGroup)

--- sdur.js
+++ sdur.js
@@ -1,10 +1,15 @@
+let targetGroup = "TargetGroup.KiDS";
+
+function selectTargetGroup(value) {
+  targetGroup = value;
+}
+
 function writeSdurEventData() {
   const id = "Event_" + Math.random().toString(36).substr(2, 9);
   const name = document.getElementById("nameField").value ?? null;
   const description = document.getElementById("descriptionField").value;
   const dateTime =
     document.getElementById("dateTimeField").value + "T00:00:00.000Z";
-  const targetGroup = document.getElementById("targetGroupField").value;
   const url = document.getElementById("urlField").value;
 
   if (name != "" && dateTime != "T00:00:00.000Z") {
