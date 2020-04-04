447d8eadc8d38d972bb2b124751291427caecfdf
David Öhman <dohman_92@hotmail.com>
2020-04-04 14:43:11 +0200

    Add Material Design and prevent adding event without title or date

--- sdur.js
+++ sdur.js
@@ -1,22 +1,19 @@
-const ID = function() {
-  return (
-    "Event_" +
-    Math.random()
-      .toString(36)
-      .substr(2, 9)
-  );
-};
-
 function writeSdurEventData() {
-  firebase
-    .database()
-    .ref(ID())
-    .set({
-      title: document.getElementById("nameField").value,
-      description: document.getElementById("descriptionField").value,
-      dateTime:
-        document.getElementById("dateTimeField").value + "T00:00:00.000Z",
-      targetGroup: document.getElementById("targetGroupField").value,
-      url: document.getElementById("urlField").value
+  const id = "Event_" + Math.random().toString(36).substr(2, 9);
+  const name = document.getElementById("nameField").value ?? null;
+  const description = document.getElementById("descriptionField").value;
+  const dateTime =
+    document.getElementById("dateTimeField").value + "T00:00:00.000Z";
+  const targetGroup = document.getElementById("targetGroupField").value;
+  const url = document.getElementById("urlField").value;
+
+  if (name != "" && dateTime != "T00:00:00.000Z") {
+    firebase.database().ref(id).set({
+      title: name,
+      description: description,
+      dateTime: dateTime,
+      targetGroup: targetGroup,
+      url: url,
     });
+  }
 }
